4e15393edc6ac841adb2579f324aac63a3e42684
mati012 <82298162+mati012@users.noreply.github.com>
2024-06-19 08:54:48 -0400

    prueba deploy

--- scripts/loadData.js
+++ scripts/loadData.js
@@ -10,13 +10,13 @@ import { fileURLToPath } from 'url';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCqRDjKg6xJ3DgQ36hsNuh6SIrkBkdx6SM",
-  authDomain: "desafio-andes.firebaseapp.com",
-  projectId: "desafio-andes",
-  storageBucket: "desafio-andes.appspot.com",
-  messagingSenderId: "542316852358",
-  appId: "1:542316852358:web:0ecc2c76c768de2a990d07",
-  measurementId: "G-FMQC7L7DY2"
+  apiKey: "AIzaSyBdfysuig7r5PSPhOqHoj6C6iBrFrF2EEU",
+  authDomain: "desafio-tecnico-581b7.firebaseapp.com",
+  projectId: "desafio-tecnico-581b7",
+  storageBucket: "desafio-tecnico-581b7.appspot.com",
+  messagingSenderId: "256787205278",
+  appId: "1:256787205278:web:fa1839b4bf60cf0792ff8f",
+  measurementId: "G-MYKB7D1SHY"
 };
 
 // Initialize Firebase
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -8,19 +8,18 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCqRDjKg6xJ3DgQ36hsNuh6SIrkBkdx6SM",
-  authDomain: "desafio-andes.firebaseapp.com",
-  projectId: "desafio-andes",
-  storageBucket: "desafio-andes.appspot.com",
-  messagingSenderId: "542316852358",
-  appId: "1:542316852358:web:0ecc2c76c768de2a990d07",
-  measurementId: "G-FMQC7L7DY2"
+  apiKey: "AIzaSyBdfysuig7r5PSPhOqHoj6C6iBrFrF2EEU",
+  authDomain: "desafio-tecnico-581b7.firebaseapp.com",
+  projectId: "desafio-tecnico-581b7",
+  storageBucket: "desafio-tecnico-581b7.appspot.com",
+  messagingSenderId: "256787205278",
+  appId: "1:256787205278:web:fa1839b4bf60cf0792ff8f",
+  measurementId: "G-MYKB7D1SHY"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-
+const analytics = getAnalytics(app);
 const db= getFirestore(app);
 
 export {db};
